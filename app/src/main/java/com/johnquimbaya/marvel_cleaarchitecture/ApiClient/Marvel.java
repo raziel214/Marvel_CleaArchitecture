12a7042d5ac458286de31c652b4aa58f4b733825
raziel214 <raziel214@users.noreply.github.com>
2018-10-07 13:17:03 -0300

    Setting Api client
    configuración del api client

--- app/src/main/java/com/johnquimbaya/marvel_cleaarchitecture/ApiClient/Marvel.java
+++ app/src/main/java/com/johnquimbaya/marvel_cleaarchitecture/ApiClient/Marvel.java
@@ -1,5 +1,26 @@
 package com.johnquimbaya.marvel_cleaarchitecture.ApiClient;
 
+import com.johnquimbaya.marvel_cleaarchitecture.Home.Model.ResultsItem;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface Marvel {
+
+    String BASE_URL="https://gateway.marvel.com/";
+
+    String API_KEY_KEY="apikey";
+    String API_KEY_VALUE="81a740022689a4eff215f0bf8c95448c";
+
+    String TIME_STAMP_KEY="ts";
+    String TIME_STAMP_VALUE="1";
+
+    String HASH_KEY="hash";
+    String HASH_VALUE="696dfe8f0aea9b17e522ceaf66ffc26e";
+
+
+    @GET("v1/public/series/{seriesID}/characters?")
+    Call<ResultsItem>getDataComic();
+
 
 }
--- app/src/main/java/com/johnquimbaya/marvel_cleaarchitecture/ApiClient/MarvelService.java
+++ app/src/main/java/com/johnquimbaya/marvel_cleaarchitecture/ApiClient/MarvelService.java
@@ -1,5 +1,50 @@
 package com.johnquimbaya.marvel_cleaarchitecture.ApiClient;
 
+import java.io.IOException;
+
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MarvelService {
+
+    public static Marvel getMarvelApi(){
+
+        OkHttpClient client= new OkHttpClient.Builder()
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+
+                        Request originalRequest= chain.request();
+                        HttpUrl originalUrl=originalRequest.url();
+                        HttpUrl httpUrl=originalUrl.newBuilder()
+                                .addQueryParameter(Marvel.API_KEY_KEY,Marvel.API_KEY_VALUE)
+                                .addQueryParameter(Marvel.TIME_STAMP_KEY, Marvel.TIME_STAMP_VALUE)
+                                .addQueryParameter(Marvel.HASH_KEY,Marvel.HASH_VALUE)
+                                .build();
+                        Request.Builder requestBuilder=originalRequest.newBuilder().url(httpUrl);
+
+                        Request request=requestBuilder.build();
+                        return chain.proceed(request);
+                    }
+                }).build();
+
+        return new Retrofit.Builder().
+                client(client).
+                baseUrl(Marvel.BASE_URL).
+                addConverterFactory(GsonConverterFactory.create()).
+                build().
+                create(Marvel.class);
+
+
+
+
+    }
+
+
 
 }
